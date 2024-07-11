3ceec031b13b7de56d5d71e38cbcdafaadfb88a4
Aaaa030 <dedpasshtet@gmail.com>
2024-07-11 19:53:51 +0500

    dz3.py

--- dz3.py
+++ dz3.py
@@ -1,8 +1,8 @@
 name = "aaa"
-print(name)
+print("Name:",name)
 age=25
-print(age)
+print("Age:",age)
 age=age+2
-print(age)
+print("New Age:",age)
 is_student=True
-print(is_student)
+print("Is_student:",is_student)
